051f7758cd637cbd9dbd6e18fec625e5b6f0711c
TJ Ferry <ferryt9@students.rowan.edu>
2019-09-13 21:16:08 -0400

    Automatically get current year results

--- js/home.js
+++ js/home.js
@@ -1,11 +1,6 @@
 var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1xqGTbkgosPqSRCkZ6xKj1c01sRRZkg0qeNeN2hrkFSI/pubhtml?gid=1847002595';
 var bandsTemplate = Handlebars.compile($('#bands-template').html());
-
-var searchTerm = 2019;
-var today = new Date();
-var nyd = new Date(today.getFullYear() + 1, 0, 1);
-var one_day = 1000 * 60 * 60 * 24;
-
+var searchTerm = new Date().getFullYear();
 loadResults(createSQL(searchTerm), mySpreadsheet);
 function createSQL(term) {
 	return "select A,B,C,D,E,F,M,L,V,W,G,H,I,J,K,X,Q,R,S,T where (A like '%" + term + "%') or (B like '" + term + "') or (lower(C) like lower('%" + term + "%')) or (lower(D) like lower('%" + term + "%')) or (lower(E) like lower('%" + term + "%')) or (F like '%" + term + "%') or (lower(O) like lower('%" + term + "%')) order by A desc, B asc";
@@ -19,6 +14,7 @@ function loadResults(sql, sheetURL) {
 		callback: function(error, options, response) {
 			if (!error) {
 				$('#bands').tablesorter();
+				document.getElementById("results-header").innerHTML = searchTerm + " Results"
 				if ($('#bands tr').length == 1) {
 					$('#bands').append("<h3>No results.</h3>")
 				}
