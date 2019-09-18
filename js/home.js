7ac2a39cb0c802ce449d45acb2d1d2fcb0a952d9
TJ Ferry <ferryt9@students.rowan.edu>
2019-09-17 21:01:14 -0400

    Load random video from the archives

--- js/home.js
+++ js/home.js
@@ -1,15 +1,13 @@
-var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1xqGTbkgosPqSRCkZ6xKj1c01sRRZkg0qeNeN2hrkFSI/pubhtml?gid=1847002595';
+var resultsSpreadsheet = 'https://docs.google.com/spreadsheets/d/1xqGTbkgosPqSRCkZ6xKj1c01sRRZkg0qeNeN2hrkFSI/pubhtml?gid=1847002595';
+var videoSpreadsheet = 'https://docs.google.com/spreadsheets/d/1JaOe5PF9YC_A6c5ntP2YKPdGvTNqruq4ohsmSvlnK1U/pubhtml?gid=1953654254';
 var bandsTemplate = Handlebars.compile($('#bands-template').html());
 var searchTerm = new Date().getFullYear();
-loadResults(createSQL(searchTerm), mySpreadsheet);
-function createSQL(term) {
-	return "select A,B,C,D,E,F,M,L,V,W,G,H,I,J,K,X,Q,R,S,T where (A like '%" + term + "%') or (B like '" + term + "') or (lower(C) like lower('%" + term + "%')) or (lower(D) like lower('%" + term + "%')) or (lower(E) like lower('%" + term + "%')) or (F like '%" + term + "%') or (lower(O) like lower('%" + term + "%')) order by A desc, B asc";
-}
+loadResults();
 
-function loadResults(sql, sheetURL) {
+function loadResults() {
 	$('#bands').sheetrock({
-		url: sheetURL,
-		query: sql,
+		url: resultsSpreadsheet,
+		query: "select A,B,C,D,E,F,M,L,V,W,G,H,I,J,K,X,Q,R,S,T where (A like '%" + searchTerm + "%') order by A desc, B asc",
 		rowTemplate: bandsTemplate,
 		callback: function(error, options, response) {
 			if (!error) {
@@ -25,11 +23,27 @@ function loadResults(sql, sheetURL) {
 	});
 }
 
+function loadRandom(){
+	$('#themes').sheetrock({
+		url: videoSpreadsheet,
+		query: "select A,B order by A desc",
+		callback: function(error, options, response) {
+			if (error) {
+		    return;
+		  }
+			randomVid = chance.pickone(response.rows);
+		  randomVid = randomVid.cellsArray[1];
+			document.getElementById('video').innerHTML = '<iframe class="embed-responsive-item" src="' + randomVid + '" allowfullscreen></iframe>'
+		}
+	});
+}
+
 Handlebars.registerHelper("normalize", function(input) {
 	return input.toLowerCase().replace(/ +/g, "+").replace(/\\.+|,.+|'.+/g, "");
 });
 
 $(document).ready(function() {
+	loadRandom();
 	$("td.note:contains('dq')").siblings(".prize").addClass("dq");
 	if ($(".dq").length != 0) {
 		$(".dqNote").show();
